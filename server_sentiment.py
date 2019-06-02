a37f29adc42fafea0b9ec07e3e3a317df2eea500
koumorikun <turbohuynh@gmail.com>
2019-06-02 00:48:00 -0700

    make coff_map construction faster

--- server_sentiment.py
+++ server_sentiment.py
@@ -25,8 +25,8 @@ def read_files(tarfname):
             trainname = member.name
         elif 'dev.tsv' in member.name:
             devname = member.name
-            
-            
+
+
     class Data: pass
     sentiment = Data()
     print("-- train data")
@@ -76,20 +76,20 @@ def read_unlabeled(tarfname, sentiment):
     class Data: pass
     unlabeled = Data()
     unlabeled.data = []
-    
+
     unlabeledname = "unlabeled.tsv"
     for member in tar.getmembers():
         if 'unlabeled.tsv' in member.name:
             unlabeledname = member.name
-            
+
     print(unlabeledname)
     tf = tar.extractfile(unlabeledname)
     for line in tf:
         line = line.decode("utf-8")
         text = line.strip()
         unlabeled.data.append(text)
-        
-            
+
+
     #unlabeled.X = sentiment.count_vect.transform(unlabeled.data)
     unlabeled.X = sentiment.tfidf_vect.transform(unlabeled.data)
 
@@ -134,14 +134,15 @@ def first_classification_task(unlabeled, cls, sentiment):
     yp = cls.predict(unlabeled.X)
     scores = cls.predict_proba(unlabeled.X)
     labels = sentiment.le.inverse_transform(yp)
-    
+
     coefficients = cls.coef_[0]
     k = 40
-    top_k =np.argsort(coefficients)[-k:]
+    # top_k =np.argsort(coefficients)[-k:]
     top_k_words = []
     top_k_map = dict()
     coff_map = dict()
     top = np.argsort(coefficients)
+    top_k = top[-k:]
     for i in top:
         coff_map[sentiment.tfidf_vect.get_feature_names()[i]] = coefficients[i]
     #print('-'*50)
@@ -156,7 +157,8 @@ def first_classification_task(unlabeled, cls, sentiment):
     #print('Bottom k=%d' %k)
     #print('-'*50)
     #top_k = np.argpartition(coefficients, -k)[-k:]
-    bottom_k =np.argsort(coefficients)[:k]
+    # bottom_k =np.argsort(coefficients)[:k]
+    bottom_k = top[:k]
     bottom_k_words = []
     bottom_k_map = dict()
     #print(top_k)
@@ -164,7 +166,7 @@ def first_classification_task(unlabeled, cls, sentiment):
         #print(sentiment.tfidf_vect.get_feature_names()[i])
         bottom_k_words.append(sentiment.tfidf_vect.get_feature_names()[i])
         bottom_k_map[sentiment.tfidf_vect.get_feature_names()[i]] = coefficients[i]
-    
+
     import nltk
     from nltk.corpus import stopwords
     stopwords = set(stopwords.words('english'))
@@ -230,7 +232,7 @@ def first_classification_task(unlabeled, cls, sentiment):
        #     print("The probability of it being positive is", scores[i][1])
        # print("---------------------------------------------------------")
     return top_set, bottom_set, stopwords, top_k_map, bottom_k_map, coff_map
-    
+
 
 
 
@@ -265,7 +267,7 @@ def semi_supervised_learning(unlabeled,sentiment,f,iters):
   #  print(type(sentiment.trainX))
   #  print(type(sentiment.trainy))
     unlabeled.data_temp = unlabeled.data
-   
+
     for i in range(iters):
 
 
@@ -304,7 +306,7 @@ def lexicon_stuff():
     print("-------------------------")
     print(opinion_lexicon.negative())
 
-        
+
 
 
 def write_basic_kaggle_file(tsvfile, outfname):
@@ -430,11 +432,10 @@ if __name__ == "__main__":
             print(sentiment.trainy.shape)
             sentiment.trainX = sentiment.tfidf_vect.transform(sentiment.train_data) # transform new training data with partition addition
             cls = classify.train_classifier(sentiment.trainX,sentiment.trainy) # train a new classifier
-            ret = classify.evaluate(sentiment.devX, sentiment.devy, cls, 'dev') # evaluate on dev portion 
+            ret = classify.evaluate(sentiment.devX, sentiment.devy, cls, 'dev') # evaluate on dev portion
             validation_acc.append(ret)
         plt.plot(iterations,validation_acc)
         plt.title("dev accuracy as number of partitions of size 8000 increased and decision threshold of 3.0")
         plt.xlabel("partitions")
         plt.ylabel("accuracy")
         plt.show()
-
